e1a804a6414c77f70dc03cff86607e5a2976877d
gpfe <arcd009999@gmail.com>
2025-04-19 21:59:55 +0200

    add cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import "./App.css";
 import ProductList from "./components/ProductList";
-import { useEffect, useState } from "react";
 import { Spinner } from "@chakra-ui/react";
-import axios from "axios";
 import { Box } from "@chakra-ui/react";
+import useProducts from "./hooks/useProducts";
 
-function App({addToCart}) {
-  const [isLoading, setIsLoading] = useState(true);
-  const [products, setProducts] = useState(null);
-
-  useEffect(() => {
-    axios.get("https://fakestoreapi.com/products").then((response) => {
-      setProducts(response.data);
-      setIsLoading(false);
-    });
-  }, []);
+function App() {
+  const { isLoading, products } = useProducts()
 
   return (
     <Box pt={"2rem"}>
-      {isLoading ? <Spinner /> : <ProductList products={products} addToCart={addToCart} />}
+      {isLoading ? <Spinner /> : <ProductList products={products} />}
     </Box>
   );
 }

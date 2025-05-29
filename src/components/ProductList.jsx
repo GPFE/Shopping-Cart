cbf90a5ffeb0001caeb71727d5af45343447216c
GPFE <arcd009999@gmail.com>
2025-05-29 19:04:11 +0200

    add skeletond for better loading experience

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,34 @@
 import ProductCard from "./ProductCard";
-import { Grid, GridItem, useMediaQuery } from "@chakra-ui/react";
+import { Grid, Card,
+  CardHeader,
+  CardBody,
+  Button,
+  CardFooter,
+  Image,
+  Center,
+  Box
+, Spinner, GridItem, useMediaQuery, Skeleton } from "@chakra-ui/react";
+import useProducts from "./../hooks/useProducts";
+import { HugeiconsIcon } from "@hugeicons/react";
+import { ShoppingBasketAdd03Icon } from "@hugeicons/core-free-icons";
 
-export default function ProductList ({ products }) {
+export default function ProductList () {
   const [isWiderThan750] = useMediaQuery("(min-width: 750px)")
+  const { isLoading, products } = useProducts()
 
   return (
     <Grid
      templateColumns={isWiderThan750 ? "repeat(auto-fill, minmax(20rem, 1fr))" : "1fr"} gap="1rem">
-      {products.map((product) => (
+
+      {isLoading ? <>
+        <Skeleton endColor="orange.100" height="36.875rem" width="100%" borderRadius=".5rem" />
+        <Skeleton endColor="orange.100" height="36.875rem" width="100%" borderRadius=".5rem" />
+        <Skeleton endColor="orange.100" height="36.875rem" width="100%" borderRadius=".5rem" />
+        <Skeleton endColor="orange.100" height="36.875rem" width="100%" borderRadius=".5rem" />
+        <Skeleton endColor="orange.100" height="36.875rem" width="100%" borderRadius=".5rem" />
+        <Skeleton endColor="orange.100" height="36.875rem" width="100%" borderRadius=".5rem" />
+      </> :
+      products.map((product) => (
         <GridItem key={product.id}>
           <ProductCard product={product} />
         </GridItem>
